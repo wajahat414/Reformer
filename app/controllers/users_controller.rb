11d17830a288a888291611793bb04b26fb24b8f2
wajahat414 <wajahatakram414@gmail.com>
2021-09-30 01:54:43 +0500

    completed module validation errors notifications on Views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,23 @@ class UsersController < ApplicationController
     def new
         @user=User.new
     end
+
+    def edit
+        @user=User.find(params[:id])
+    end
+
+    def update
+        @user=User.find(params[:id]) 
+        @user.update!(user_params) 
+        redirect_to @user
+    end
+
+    def show
+        @user=User.find(params[:id])
+    end
+
+
+
     def create
         @user=User.new(user_params)
        # @user=User.new(username: params[:username],email: params[:email],password: params[:password])
